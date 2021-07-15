9de44b316c2022af8680304a60589a05d2a43429
jaydanrev28 <jaydan.jacobs@revature.net>
2021-07-15 10:35:03 -0400

    Day 4 Lab

---
+++ Lab-Method/src/MethodDeclaration.java
@@ -0,0 +1,13 @@
+
+public class MethodDeclaration {
+	public static void main(String[] args){
+	    //create a class instance
+	    MethodDeclaration md = new MethodDeclaration();
+
+	    //call your first method here
+	
+	    md.talk();
+	    md.talk();
+	    md.talk();
+	}
+	}
