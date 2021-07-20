441736907d40d4a5a8a2bfef8f6662d1f9b8505c
jaydanrev28 <jaydan.jacobs@revature.net>
2021-07-19 20:10:24 -0400

    Day 5

---
+++ Lab-Recursion/src/ExampleOne.java
@@ -0,0 +1,20 @@
+
+public class ExampleOne {
+	public static void main (String[] args) {
+      
+        int input = 180;
+
+        ExampleOne eo = new ExampleOne();
+
+        
+        System.out.println(eo.factorial(input));
+    }
+
+    
+    public int factorial(int num) {
+        if (num <= 0) return 0;
+        if (num == 1) return 1;
+
+        return num * factorial(num-1);
+}
+}
